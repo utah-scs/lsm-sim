80f292e02c405cb678ec5b03029b17ff0a8c050a
Daniel Rushton <daniel.r.rushton@gmail.com>
2016-02-12 16:24:04 -0700

    added a script to a bunch of app scripts, output the results with timestamped files, and keeps a timestamped log with output from stdout and stderr

--- trace_script/trace_script.sh
+++ trace_script/trace_script.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
 
+OUTPUT="/home/drushton/projects/lsm-results/"
+TIME=$(date +"%m_%d_%y_%T") 
+LOG=$OUTPUT/$TIME.log
+
+exec  > >(tee -a $LOG)
+exec 2> >(tee -a $LOG >&2)
+
 run_glru () {
 
+  INPUT=$1
+  APPID=${INPUT##*/}  
   POLICY="gLRU"
-  TIME=$(date +"%m_%d_%y_%T")
-  FILENAME=$TRACENAME.$POLICY.$TIME
-  printf "Processing trace: %s policy: %s %s\n" $TRACENAME $POLICY $TIME
-  # ./compute -p 3 -f $1 -o $FILENAME
+  TIME=$(date +"%m_%d_%y_%T") 
+  FILENAME=$TIME.$APPID.$POLICY
+ 
+  printf "%s   %s  %s\n" $TIME $APPID $POLICY
+  ../compute -p 0 -f $1 
+  mv shadowlru-size-curve.data $OUTPUT$FILENAME."size-curve.data"
+  mv shadowlru-position-curve.data $OUTPUT$FILENAME."position-curve.data"
+
 }
 
-run_shadow_slab () {
+run_slab () {
 
-  POLICY="SSlb"
+  INPUT=$1
+  APPID=${INPUT##*/}  
+  POLICY="sSLB"
   TIME=$(date +"%m_%d_%y_%T")
-  FILENAME=$TRACENAME.$POLICY.$TIME
-  printf "Processing trace: %s policy: %s %s\n" $TRACENAME $POLICY $TIME
-  # ./compute -p 4 -f $1 -o $FILENAME
+  APPEND="size-curve.data"
+  FILENAME=$TIME.$APPID.$POLICY."size-curve.data"
+  
+  printf "%s   %s   %s\n" $TIME $APPID $POLICY
+  ../compute -p 4 -f $1
+  mv shadowslab-size-curve.data $OUTPUT$FILENAME
 }
 
 
 # ... add rules for other policies here
 
 # For all input traces
-TRACES=./traces/*;
+echo "date/time(start)  app ID  policy"
+echo "---------------------------------"
+#TRACES=/mnt/data/projects/lsm-traces/*;
+TRACES=~/test_traces/*;
 for f in $TRACES
 do
-  TRACENAME=${f##.*/} 
-  run_glru $TRACENAME
-  run_shadow_slab $TRACENAME 
+  if [[ "${f/app}" != "$f" ]];
+  then
+    run_glru $f
+    echo 
+    run_slab $f
+    echo
+  fi
 done
 
-TIME=$(date +"%m_%d_.%T")
+TIME=$(date +"%m_%d_%T")
 
 echo $TIME "done."
 
