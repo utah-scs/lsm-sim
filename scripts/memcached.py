cddb15beee7e0c245e109769ee759d04fdcff395
Daniel Rushton <daniel.r.rushton@gmail.com>
2016-03-13 16:12:12 -0600

    added request class and parsing into

--- scripts/memcached.py
+++ scripts/memcached.py
@@ -1,9 +1,13 @@
 #!python
 
+import sys
 import csv
+
 
 from collections import deque
 
+
+  
 
 trace_file = sys.argv[1]
 
@@ -12,6 +16,27 @@ trace_file = sys.argv[1]
 hits = 0
 misses = 0
 
+class Request:
+  def __init__ (self, time, appid, typ, ksz, vsz, kid, hit):
+    self.time = time
+    self.appid = appid
+    self.typ = typ
+    self.ksz = ksz
+    self.vsz = vsz
+    self.kid = kid
+    self.hit = hit
+  def printReq (self) :
+    print "%f %d %d %d %d %d %d" %(self.time, self.appid, self.typ, self.ksz, 
+    self.vsz, self.kid, self.hit) 
+
+with open(trace_file, 'rb') as tf:
+  reader = csv.reader(tf, delimiter=',')
+  for rw in reader:
+    req = Request(float(rw[0]),int(rw[1]),int(rw[2]),int(rw[3]),int(rw[4]),
+    int(rw[5]),int(rw[6]))
+    req.printReq()   
+
+
 class SlabClass:
   def __init__(self, chk):
     self.chunk_size = chk
@@ -19,9 +44,9 @@ class SlabClass:
   eviction_queue = deque()
   hits=0
   slabs=1
-  def add_slab ():
+  def add_slab (self):
     self.slabs.append(Slab(self.chunk_size, ++slabs))
-  def kill_slab (sid):
+  def kill_slab (self,sid):
     self.slabs.remove(sid)
 
 class Slab:
@@ -37,10 +62,13 @@ class Slab:
     return 1 - (bytes_cached/(objs_cached * chunk_size))
 
 class DataObj:
-  def __init__(self, ksz, vsz)
+  def __init__(self, ksz, vsz, objid):
     self.ksz = ksz
     self.vsz = vsz
-  def size ():
+    self.objid = objid
+  def __eq__(self, other):
+    return self.objid == other.objid
+  def size (self):
     return ksz + vsz
-
+  
 
